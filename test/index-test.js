76e73127dedc4f217301d8e13173d8de42c21258
Sean Hendry <sean.c.hendry@gmail.com>
2018-11-15 23:52:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,6 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
-  expect(greeting).toEqual("Hello, everybody!")
+var greeting="Hello, everybody!"
 })
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
